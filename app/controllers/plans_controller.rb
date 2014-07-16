5c3f5daea5644c3e9fcf43fa8d6e9cc828819fb8
Jaime Victoria <jaime.victoria.90@gmail.com>
2014-07-15 19:57:35 -0500

    deleted unnecessary files and added resources

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,2 +1,11 @@
 class PlansController < ApplicationController
+  def index
+    plans = Plan.all
+    render json: plans, status: :ok
+  end
+
+  def show
+    plan = Plan.find(params[:id])
+    render json: plan, status: :ok
+  end
 end
